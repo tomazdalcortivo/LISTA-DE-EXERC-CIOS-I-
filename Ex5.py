3716a6108b1b029f069d73560606d749f2fc877a
tomazdalcortivo <p12547254@gmail.com>
2024-07-20 17:46:03 -0300

    resolvendo ex 5

---
+++ Ex5.py
@@ -0,0 +1,6 @@
+distanciaTotal = float(input('informe a distancia total percorida '))
+totalCombustivelGasto = float(input('informe o total de combustivel gasto '))
+
+consumoMedio = distanciaTotal / totalCombustivelGasto
+
+print(f'O consumo medio é {consumoMedio}')
