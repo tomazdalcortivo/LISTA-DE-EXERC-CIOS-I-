a32c55729d01a1b4cb2a479bd361a7eec83c1760
tomazdalcortivo <p12547254@gmail.com>
2024-07-20 17:38:59 -0300

    resolvendo ex 4

---
+++ Ex4.py
@@ -0,0 +1,7 @@
+comp = float(input('Digite o comprimento da caixa '))
+larg = float(input('Digite o largura da caixa '))
+alt = float(input('Digite o altura da caixa '))
+ 
+volume = comp * larg * alt 
+
+print(f'O volume do retangulo foi {volume}')
