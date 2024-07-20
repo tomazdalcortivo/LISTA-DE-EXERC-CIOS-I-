89cb165f84ee5bc32fa7b0b367c5c8e0728b9e7c
tomazdalcortivo <p12547254@gmail.com>
2024-07-20 19:36:39 -0300

    resolvendo ex 12

---
+++ Ex12.py
@@ -0,0 +1,5 @@
+quntChuvaPolegas = float(input('Informe a quantidade de chuva em polegadas '))
+
+quantChuvamm = quntChuvaPolegas * 25.4
+
+print(f'A quantidade de chuva em milímetros é: {quantChuvamm:.2f} mm')
