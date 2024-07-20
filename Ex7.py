a88c33c190fac21951ed559aa9cfe7970579c6ed
tomazdalcortivo <p12547254@gmail.com>
2024-07-20 19:35:41 -0300

    resolvendo ex 7

---
+++ Ex7.py
@@ -0,0 +1,7 @@
+p1 = float(input('informe a nota da primeira prova '))
+p2 = float(input('informe a nota da segunda prova '))
+p3 = float(input('informe a nota da terceira prova '))
+
+mediaFinal = ((p1 * 2) + (p2 * 4) + (p3 * 6)) / 12
+
+print(f'A media final do aluno foi {mediaFinal:.2f}')
