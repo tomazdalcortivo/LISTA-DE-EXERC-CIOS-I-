047ffb09a570c3826922eaff707b9e3d6b75deb3
tomazdalcortivo <p12547254@gmail.com>
2024-07-20 19:36:45 -0300

    resolvendo ex 13

---
+++ Ex13.py
@@ -0,0 +1,9 @@
+num1 = float(input('Informe o primeiro numero '))
+num2 = float(input('Informe o segundo numero '))
+num3 = float(input('Informe o terceiro numero '))
+num4 = float(input('Informe o quarto numero '))
+
+
+media = (num1 + num2 + num3 + num4) / 4
+
+print(f' O resultado da media dos quatro valores foi {media}')
