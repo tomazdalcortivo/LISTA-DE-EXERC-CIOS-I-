4f0fa6bc5951bb80e2b81b1d83ada5572cccaf2e
Pedro Tomaz <p12547254@gmail.com>
2024-07-22 20:20:28 -0300

    resolvendo ex 23

---
+++ Ex23.py
@@ -0,0 +1,5 @@
+temp_celsius = float(input("Digite a temperatura em Celsius: "))
+
+temp_estevam = 1.3 * temp_celsius + 20
+
+print(f"A temperatura de {temp_celsius}°C equivale a {temp_estevam} na escala Estevam.")
