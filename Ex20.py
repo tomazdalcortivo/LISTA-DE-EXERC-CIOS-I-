181a786f14d77dddd350757a331ce4b14631f60a
tomazdalcortivo <p12547254@gmail.com>
2024-07-22 18:35:22 -0300

    resolvendo ex 20

---
+++ Ex20.py
@@ -0,0 +1,7 @@
+import math 
+
+angulo_graus = float(input("Digite o angulo em graus a ser convertido "))
+
+angulo_rad = angulo_graus * (math.pi / 180)
+
+print(f"O ângulo de {angulo_graus} graus é equivalente a {angulo_rad} radianos.")
