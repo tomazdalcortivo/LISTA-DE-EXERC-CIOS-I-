9f13a2c750c4ac5c7f3229c19f62a83ea0c457fe
tomazdalcortivo <p12547254@gmail.com>
2024-07-23 16:08:05 -0300

    resolvendo ex 29

---
+++ Ex29.py
@@ -0,0 +1,7 @@
+idade = int(input("Informe a idade: "))
+altura = float(input("Informe a altura: "))
+
+if idade > 18 and altura > 1.90:
+    print("O jogador está fora da categora A")
+else:
+    print("O jogador está dentro da categora A")
